46e5acf71856d03181195ad8d01f54757ed90008
oblivionkid56-crypto <oblivionkid56@gmail.com>
2025-12-01 17:15:51 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -84,8 +84,10 @@ function isAdmin(user) {
 
 // Handles leetspeak, numbers, spacing, mixing words, etc.
 const bannedWordPatterns = [
-  /n[i1!|]g+[e3]?r/i,
-  /negr[o0]/i,          // ← ADD THIS LINE
+  // ULTIMATE N-WORD BLOCKER
+  /n[\W_]*[i1l!|][\W_]*[gq9]+[\W_]*[gq9]+[\W_]*[ae@4]?[\W_]*r?/i,
+
+  // Other slurs & profanity
   /f[a4]g+[o0]?t/i,
   /rap[e3]/i,
   /ped[o0]/i,
